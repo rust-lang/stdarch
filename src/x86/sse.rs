83b9b0a6f4c0c454654855d773e7ea128e4c82a3
Thomas Schilling <nominolo@googlemail.com>
2017-10-07 22:49:03 +0200

    Add _mm_cmple_ss

--- src/x86/sse.rs
+++ src/x86/sse.rs
@@ -232,6 +232,17 @@ pub unsafe fn _mm_cmplt_ss(a: f32x4, b: f32x4) -> f32x4 {
     cmpss(a, b, 1)
 }
 
+/// Compare the lowest `f32` of both inputs for less than or equal. The lowest
+/// 32 bits of the result will be `0xffffffff` if `a.extract(0)` is less than or
+/// equal `b.extract(0)`, or `0` otherwise. The upper 96 bits of the result are
+/// the upper 96 bits of `a`.
+#[inline(always)]
+#[target_feature = "+sse"]
+#[cfg_attr(test, assert_instr(cmpless))]
+pub unsafe fn _mm_cmple_ss(a: f32x4, b: f32x4) -> f32x4 {
+    cmpss(a, b, 2)
+}
+
 /// Construct a `f32x4` with the lowest element set to `a` and the rest set to
 /// zero.
 #[inline(always)]
@@ -1210,6 +1221,32 @@ mod tests {
         assert_eq!(rc, ec);
 
         let rd: u32x4 = transmute(sse::_mm_cmplt_ss(a, d));
+        let ed: u32x4 = transmute(f32x4::new(transmute(d1), 2.0, 3.0, 4.0));
+        assert_eq!(rd, ed);
+    }
+
+    #[simd_test = "sse"]
+    unsafe fn _mm_cmple_ss() {
+        use std::mem::transmute;
+
+        let a = f32x4::new(1.0, 2.0, 3.0, 4.0);
+        let b = f32x4::new(0.0, 5.0, 6.0, 7.0);
+        let c = f32x4::new(1.0, 5.0, 6.0, 7.0);
+        let d = f32x4::new(2.0, 5.0, 6.0, 7.0);
+
+        let b1 = 0u32;  // a.extract(0) <= b.extract(0)
+        let c1 = !0u32;  // a.extract(0) <= c.extract(0)
+        let d1 = !0u32;  // a.extract(0) <= d.extract(0)
+
+        let rb: u32x4 = transmute(sse::_mm_cmple_ss(a, b));
+        let eb: u32x4 = transmute(f32x4::new(transmute(b1), 2.0, 3.0, 4.0));
+        assert_eq!(rb, eb);
+
+        let rc: u32x4 = transmute(sse::_mm_cmple_ss(a, c));
+        let ec: u32x4 = transmute(f32x4::new(transmute(c1), 2.0, 3.0, 4.0));
+        assert_eq!(rc, ec);
+
+        let rd: u32x4 = transmute(sse::_mm_cmple_ss(a, d));
         let ed: u32x4 = transmute(f32x4::new(transmute(d1), 2.0, 3.0, 4.0));
         assert_eq!(rd, ed);
     }
