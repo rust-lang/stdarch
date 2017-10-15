502a814ff807eae466f46ca4f4c9905a34cf28b6
gwenn <gtreguier@gmail.com>
2017-10-15 13:13:23 +0200

    avx: _mm256_set_epi8

--- src/x86/avx.rs
+++ src/x86/avx.rs
@@ -1644,6 +1644,28 @@ pub unsafe fn _mm256_set_ps(a: f32, b: f32, c: f32, d: f32,
     f32x8::new(h, g, f, e, d, c, b, a)
 }
 
+/// Set packed 8-bit integers in returned vector with the supplied values in
+/// reverse order.
+#[inline(always)]
+#[target_feature = "+avx"]
+pub unsafe fn _mm256_set_epi8(a00: i8, a01: i8, a02: i8, a03: i8,
+                              a04: i8, a05: i8, a06: i8, a07: i8,
+                              a08: i8, a09: i8, a10: i8, a11: i8,
+                              a12: i8, a13: i8, a14: i8, a15: i8,
+                              a16: i8, a17: i8, a18: i8, a19: i8,
+                              a20: i8, a21: i8, a22: i8, a23: i8,
+                              a24: i8, a25: i8, a26: i8, a27: i8,
+                              a28: i8, a29: i8, a30: i8, a31: i8) -> i8x32 {
+    i8x32::new(a31, a30, a29, a28,
+               a27, a26, a25, a24,
+               a23, a22, a21, a20,
+               a19, a18, a17, a16,
+               a15, a14, a13, a12,
+               a11, a10, a09, a08,
+               a07, a06, a05, a04,
+               a03, a02, a01, a00)
+}
+
 /// Casts vector of type __m128 to type __m256;
 /// the upper 128 bits of the result are undefined.
 #[inline(always)]
@@ -3042,5 +3064,18 @@ mod tests {
     unsafe fn _mm256_set_ps() {
         let r = avx::_mm256_set_ps(1., 2., 3., 4., 5., 6., 7., 8.);
         assert_eq!(r, f32x8::new(8., 7., 6., 5., 4., 3., 2., 1.));
+    }
+
+    #[simd_test = "avx"]
+    unsafe fn _mm256_set_epi8() {
+        let r = avx::_mm256_set_epi8(
+            1, 2, 3, 4, 5, 6, 7, 8,
+            9, 10, 11, 12, 13, 14, 15, 16,
+            17, 18, 19, 20, 21, 22, 23, 24,
+            25, 26, 27, 28, 29, 30, 31, 32);
+        assert_eq!(r, i8x32::new(32, 31, 30, 29, 28, 27, 26, 25,
+                                 24, 23, 22, 21, 20, 19, 18, 17,
+                                 16, 15, 14, 13, 12, 11, 10, 9,
+                                 8, 7, 6, 5, 4, 3, 2, 1));
     }
 }
