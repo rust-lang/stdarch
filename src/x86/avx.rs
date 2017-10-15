f4ca21e63621e7485b5ea28de6dd9e9178f777db
gwenn <gtreguier@gmail.com>
2017-10-15 09:20:24 +0200

    avx: _mm256_testz_ps, _mm256_testc_ps, _mm256_testnzc_ps

--- src/x86/avx.rs
+++ src/x86/avx.rs
@@ -1499,6 +1499,49 @@ pub unsafe fn _mm_testnzc_pd(a: f64x2, b: f64x2) -> i32 {
     vtestnzcpd(a, b)
 }
 
+/// Compute the bitwise AND of 256 bits (representing single-precision (32-bit)
+/// floating-point elements) in `a` and `b`, producing an intermediate 256-bit
+/// value, and set `ZF` to 1 if the sign bit of each 32-bit element in the
+/// intermediate value is zero, otherwise set `ZF` to 0. Compute the bitwise
+/// NOT of `a` and then AND with `b`, producing an intermediate value, and set
+/// `CF` to 1 if the sign bit of each 32-bit element in the intermediate value
+/// is zero, otherwise set `CF` to 0. Return the `ZF` value.
+#[inline(always)]
+#[target_feature = "+avx"]
+#[cfg_attr(test, assert_instr(vtestps))]
+pub unsafe fn _mm256_testz_ps(a: f32x8, b: f32x8) -> i32 {
+    vtestzps256(a, b)
+}
+
+/// Compute the bitwise AND of 256 bits (representing single-precision (32-bit)
+/// floating-point elements) in `a` and `b`, producing an intermediate 256-bit
+/// value, and set `ZF` to 1 if the sign bit of each 32-bit element in the
+/// intermediate value is zero, otherwise set `ZF` to 0. Compute the bitwise
+/// NOT of `a` and then AND with `b`, producing an intermediate value, and set
+/// `CF` to 1 if the sign bit of each 32-bit element in the intermediate value
+/// is zero, otherwise set `CF` to 0. Return the `CF` value.
+#[inline(always)]
+#[target_feature = "+avx"]
+#[cfg_attr(test, assert_instr(vtestps))]
+pub unsafe fn _mm256_testc_ps(a: f32x8, b: f32x8) -> i32 {
+    vtestcps256(a, b)
+}
+
+/// Compute the bitwise AND of 256 bits (representing single-precision (32-bit)
+/// floating-point elements) in `a` and `b`, producing an intermediate 256-bit
+/// value, and set `ZF` to 1 if the sign bit of each 32-bit element in the
+/// intermediate value is zero, otherwise set `ZF` to 0. Compute the bitwise
+/// NOT of `a` and then AND with `b`, producing an intermediate value, and set
+/// `CF` to 1 if the sign bit of each 32-bit element in the intermediate value
+/// is zero, otherwise set `CF` to 0. Return 1 if both the `ZF` and `CF` values
+/// are zero, otherwise return 0.
+#[inline(always)]
+#[target_feature = "+avx"]
+#[cfg_attr(test, assert_instr(vtestps))]
+pub unsafe fn _mm256_testnzc_ps(a: f32x8, b: f32x8) -> i32 {
+    vtestnzcps256(a, b)
+}
+
 /// Casts vector of type __m128 to type __m256;
 /// the upper 128 bits of the result are undefined.
 #[inline(always)]
@@ -1674,6 +1717,12 @@ extern "C" {
     fn vtestcpd(a: f64x2, b: f64x2) -> i32;
     #[link_name = "llvm.x86.avx.vtestnzc.pd"]
     fn vtestnzcpd(a: f64x2, b: f64x2) -> i32;
+    #[link_name = "llvm.x86.avx.vtestz.ps.256"]
+    fn vtestzps256(a: f32x8, b: f32x8) -> i32;
+    #[link_name = "llvm.x86.avx.vtestc.ps.256"]
+    fn vtestcps256(a: f32x8, b: f32x8) -> i32;
+    #[link_name = "llvm.x86.avx.vtestnzc.ps.256"]
+    fn vtestnzcps256(a: f32x8, b: f32x8) -> i32;
 }
 
 #[cfg(test)]
@@ -2774,6 +2823,37 @@ mod tests {
         let a = f64x2::new(1., -1.);
         let b = f64x2::new(-1., -1.);
         let r = avx::_mm_testnzc_pd(a, b);
+        assert_eq!(r, 1);
+    }
+
+    #[simd_test = "avx"]
+    unsafe fn _mm256_testz_ps() {
+        let a = f32x8::splat(1.);
+        let r = avx::_mm256_testz_ps(a, a);
+        assert_eq!(r, 1);
+        let a = f32x8::splat(-1.);
+        let r = avx::_mm256_testz_ps(a, a);
+        assert_eq!(r, 0);
+    }
+
+    #[simd_test = "avx"]
+    unsafe fn _mm256_testc_ps() {
+        let a = f32x8::splat(1.);
+        let r = avx::_mm256_testc_ps(a, a);
+        assert_eq!(r, 1);
+        let b = f32x8::splat(-1.);
+        let r = avx::_mm256_testc_ps(a, b);
+        assert_eq!(r, 0);
+    }
+
+    #[simd_test = "avx"]
+    unsafe fn _mm256_testnzc_ps() {
+        let a = f32x8::splat(1.);
+        let r = avx::_mm256_testnzc_ps(a, a);
+        assert_eq!(r, 0);
+        let a = f32x8::new(1., -1., -1., -1., -1., -1., -1., -1.);
+        let b = f32x8::new(-1., -1., 1., 1., 1., 1., 1., 1.);
+        let r = avx::_mm256_testnzc_ps(a, b);
         assert_eq!(r, 1);
     }
 }
