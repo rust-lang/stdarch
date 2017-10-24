f5f129f331eca13be0256280298a8d53421b59b7
Mrowqa <artur.jamro@gmail.com>
2017-10-24 22:57:11 +0200

    x86: fixed testing equality of floating point numbers

--- src/x86/macros.rs
+++ src/x86/macros.rs
@@ -338,4 +338,22 @@ macro_rules! constify_imm2 {
     }
 }
 
+macro_rules! assert_approx_eq {
+    ($a:expr, $b:expr) => ({
+        let eps = 1.0e-6;
+        let (a, b) = (&$a, &$b);
+        assert!((*a - *b).abs() < eps,
+                "assertion failed: `(left !== right)` \
+                           (left: `{:?}`, right: `{:?}`, expect diff: `{:?}`, real diff: `{:?}`)",
+                 *a, *b, eps, (*a - *b).abs());
+    });
+    ($a:expr, $b:expr, $eps:expr) => ({
+        let (a, b) = (&$a, &$b);
+        assert!((*a - *b).abs() < $eps,
+                "assertion failed: `(left !== right)` \
+                           (left: `{:?}`, right: `{:?}`, expect diff: `{:?}`, real diff: `{:?}`)",
+                 *a, *b, $eps, (*a - *b).abs());
+    })
+}
+
 
