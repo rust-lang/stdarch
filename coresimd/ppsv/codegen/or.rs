84f4580aa50105dde6b8c6e22e83ba6f39bbee9f
gnzlbg <gonzalobg88@gmail.com>
2018-03-05 16:45:11 +0100

    rename the llvm intrinsics to use llvm names

--- coresimd/ppsv/codegen/or.rs
+++ coresimd/ppsv/codegen/or.rs
@@ -4,77 +4,77 @@ use ::coresimd::simd::*;
 /// LLVM intrinsics used in the or reduction
 #[allow(improper_ctypes)]
 extern "C" {
-    #[link_name = "llvm.experimental.vector.reduce.or.i8.i8x2"]
+    #[link_name = "llvm.experimental.vector.reduce.or.i8.v2i8"]
     fn reduce_or_i8x2(x: i8x2) -> i8;
-    #[link_name = "llvm.experimental.vector.reduce.or.u8.u8x2"]
+    #[link_name = "llvm.experimental.vector.reduce.or.u8.v2u8"]
     fn reduce_or_u8x2(x: u8x2) -> u8;
-    #[link_name = "llvm.experimental.vector.reduce.or.i16.i16x2"]
+    #[link_name = "llvm.experimental.vector.reduce.or.i16.v2i16"]
     fn reduce_or_i16x2(x: i16x2) -> i16;
-    #[link_name = "llvm.experimental.vector.reduce.or.u16.u16x2"]
+    #[link_name = "llvm.experimental.vector.reduce.or.u16.v2u16"]
     fn reduce_or_u16x2(x: u16x2) -> u16;
-    #[link_name = "llvm.experimental.vector.reduce.or.i32.i32x2"]
+    #[link_name = "llvm.experimental.vector.reduce.or.i32.v2i32"]
     fn reduce_or_i32x2(x: i32x2) -> i32;
-    #[link_name = "llvm.experimental.vector.reduce.or.u32.u32x2"]
+    #[link_name = "llvm.experimental.vector.reduce.or.u32.v2u32"]
     fn reduce_or_u32x2(x: u32x2) -> u32;
-    #[link_name = "llvm.experimental.vector.reduce.or.i64.i64x2"]
+    #[link_name = "llvm.experimental.vector.reduce.or.i64.v2i64"]
     fn reduce_or_i64x2(x: i64x2) -> i64;
-    #[link_name = "llvm.experimental.vector.reduce.or.u64.u64x2"]
+    #[link_name = "llvm.experimental.vector.reduce.or.u64.v2u64"]
     fn reduce_or_u64x2(x: u64x2) -> u64;
-    #[link_name = "llvm.experimental.vector.reduce.or.i8.i8x4"]
+    #[link_name = "llvm.experimental.vector.reduce.or.i8.v4i8"]
     fn reduce_or_i8x4(x: i8x4) -> i8;
-    #[link_name = "llvm.experimental.vector.reduce.or.u8.u8x4"]
+    #[link_name = "llvm.experimental.vector.reduce.or.u8.v4u8"]
     fn reduce_or_u8x4(x: u8x4) -> u8;
-    #[link_name = "llvm.experimental.vector.reduce.or.i16.i16x4"]
+    #[link_name = "llvm.experimental.vector.reduce.or.i16.v4i16"]
     fn reduce_or_i16x4(x: i16x4) -> i16;
-    #[link_name = "llvm.experimental.vector.reduce.or.u16.u16x4"]
+    #[link_name = "llvm.experimental.vector.reduce.or.u16.v4u16"]
     fn reduce_or_u16x4(x: u16x4) -> u16;
-    #[link_name = "llvm.experimental.vector.reduce.or.i32.i32x4"]
+    #[link_name = "llvm.experimental.vector.reduce.or.i32.v4i32"]
     fn reduce_or_i32x4(x: i32x4) -> i32;
-    #[link_name = "llvm.experimental.vector.reduce.or.u32.u32x4"]
+    #[link_name = "llvm.experimental.vector.reduce.or.u32.v4u32"]
     fn reduce_or_u32x4(x: u32x4) -> u32;
-    #[link_name = "llvm.experimental.vector.reduce.or.i64.i64x4"]
+    #[link_name = "llvm.experimental.vector.reduce.or.i64.v4i64"]
     fn reduce_or_i64x4(x: i64x4) -> i64;
-    #[link_name = "llvm.experimental.vector.reduce.or.u64.u64x4"]
+    #[link_name = "llvm.experimental.vector.reduce.or.u64.v4u64"]
     fn reduce_or_u64x4(x: u64x4) -> u64;
-    #[link_name = "llvm.experimental.vector.reduce.or.i8.i8x8"]
+    #[link_name = "llvm.experimental.vector.reduce.or.i8.v8i8"]
     fn reduce_or_i8x8(x: i8x8) -> i8;
-    #[link_name = "llvm.experimental.vector.reduce.or.u8.u8x8"]
+    #[link_name = "llvm.experimental.vector.reduce.or.u8.v8u8"]
     fn reduce_or_u8x8(x: u8x8) -> u8;
-    #[link_name = "llvm.experimental.vector.reduce.or.i16.i16x8"]
+    #[link_name = "llvm.experimental.vector.reduce.or.i16.v8i16"]
     fn reduce_or_i16x8(x: i16x8) -> i16;
-    #[link_name = "llvm.experimental.vector.reduce.or.u16.u16x8"]
+    #[link_name = "llvm.experimental.vector.reduce.or.u16.v8u16"]
     fn reduce_or_u16x8(x: u16x8) -> u16;
-    #[link_name = "llvm.experimental.vector.reduce.or.i32.i32x8"]
+    #[link_name = "llvm.experimental.vector.reduce.or.i32.v8i32"]
     fn reduce_or_i32x8(x: i32x8) -> i32;
-    #[link_name = "llvm.experimental.vector.reduce.or.u32.u32x8"]
+    #[link_name = "llvm.experimental.vector.reduce.or.u32.v8u32"]
     fn reduce_or_u32x8(x: u32x8) -> u32;
-    #[link_name = "llvm.experimental.vector.reduce.or.i64.i64x8"]
+    #[link_name = "llvm.experimental.vector.reduce.or.i64.v8i64"]
     fn reduce_or_i64x8(x: i64x8) -> i64;
-    #[link_name = "llvm.experimental.vector.reduce.or.u64.u64x8"]
+    #[link_name = "llvm.experimental.vector.reduce.or.u64.v8u64"]
     fn reduce_or_u64x8(x: u64x8) -> u64;
-    #[link_name = "llvm.experimental.vector.reduce.or.i8.i8x16"]
+    #[link_name = "llvm.experimental.vector.reduce.or.i8.v16i8"]
     fn reduce_or_i8x16(x: i8x16) -> i8;
-    #[link_name = "llvm.experimental.vector.reduce.or.u8.u8x16"]
+    #[link_name = "llvm.experimental.vector.reduce.or.u8.v16u8"]
     fn reduce_or_u8x16(x: u8x16) -> u8;
-    #[link_name = "llvm.experimental.vector.reduce.or.i16.i16x16"]
+    #[link_name = "llvm.experimental.vector.reduce.or.i16.v16i16"]
     fn reduce_or_i16x16(x: i16x16) -> i16;
-    #[link_name = "llvm.experimental.vector.reduce.or.u16.u16x16"]
+    #[link_name = "llvm.experimental.vector.reduce.or.u16.v16u16"]
     fn reduce_or_u16x16(x: u16x16) -> u16;
-    #[link_name = "llvm.experimental.vector.reduce.or.i32.i32x16"]
+    #[link_name = "llvm.experimental.vector.reduce.or.i32.v16i32"]
     fn reduce_or_i32x16(x: i32x16) -> i32;
-    #[link_name = "llvm.experimental.vector.reduce.or.u32.u32x16"]
+    #[link_name = "llvm.experimental.vector.reduce.or.u32.v16u32"]
     fn reduce_or_u32x16(x: u32x16) -> u32;
-    #[link_name = "llvm.experimental.vector.reduce.or.i8.i8x32"]
+    #[link_name = "llvm.experimental.vector.reduce.or.i8.v32i8"]
     fn reduce_or_i8x32(x: i8x32) -> i8;
-    #[link_name = "llvm.experimental.vector.reduce.or.u8.u8x32"]
+    #[link_name = "llvm.experimental.vector.reduce.or.u8.v32u8"]
     fn reduce_or_u8x32(x: u8x32) -> u8;
-    #[link_name = "llvm.experimental.vector.reduce.or.i16.i16x32"]
+    #[link_name = "llvm.experimental.vector.reduce.or.i16.v32i16"]
     fn reduce_or_i16x32(x: i16x32) -> i16;
-    #[link_name = "llvm.experimental.vector.reduce.or.u16.u16x32"]
+    #[link_name = "llvm.experimental.vector.reduce.or.u16.v32u16"]
     fn reduce_or_u16x32(x: u16x32) -> u16;
-    #[link_name = "llvm.experimental.vector.reduce.or.i8.i8x64"]
+    #[link_name = "llvm.experimental.vector.reduce.or.i8.v64i8"]
     fn reduce_or_i8x64(x: i8x64) -> i8;
-    #[link_name = "llvm.experimental.vector.reduce.or.u8.u8x64"]
+    #[link_name = "llvm.experimental.vector.reduce.or.u8.v64u8"]
     fn reduce_or_u8x64(x: u8x64) -> u8;
 }
 
--- coresimd/ppsv/codegen/product.rs
+++ coresimd/ppsv/codegen/product.rs
@@ -4,94 +4,92 @@ use ::coresimd::simd::*;
 /// LLVM intrinsics used in the product reduction
 #[allow(improper_ctypes)]
 extern "C" {
-    #[link_name = "llvm.experimental.vector.reduce.mul.i8.i8x2"]
+    #[link_name = "llvm.experimental.vector.reduce.mul.i8.v2i8"]
     fn reduce_mul_i8x2(x: i8x2) -> i8;
-    #[link_name = "llvm.experimental.vector.reduce.mul.u8.u8x2"]
+    #[link_name = "llvm.experimental.vector.reduce.mul.u8.v2u8"]
     fn reduce_mul_u8x2(x: u8x2) -> u8;
-    #[link_name = "llvm.experimental.vector.reduce.mul.i16.i16x2"]
+    #[link_name = "llvm.experimental.vector.reduce.mul.i16.v2i16"]
     fn reduce_mul_i16x2(x: i16x2) -> i16;
-    #[link_name = "llvm.experimental.vector.reduce.mul.u16.u16x2"]
+    #[link_name = "llvm.experimental.vector.reduce.mul.u16.v2u16"]
     fn reduce_mul_u16x2(x: u16x2) -> u16;
-    #[link_name = "llvm.experimental.vector.reduce.mul.i32.i32x2"]
+    #[link_name = "llvm.experimental.vector.reduce.mul.i32.v2i32"]
     fn reduce_mul_i32x2(x: i32x2) -> i32;
-    #[link_name = "llvm.experimental.vector.reduce.mul.u32.u32x2"]
+    #[link_name = "llvm.experimental.vector.reduce.mul.u32.v2u32"]
     fn reduce_mul_u32x2(x: u32x2) -> u32;
-    #[link_name = "llvm.experimental.vector.reduce.mul.i64.i64x2"]
+    #[link_name = "llvm.experimental.vector.reduce.mul.i64.v2i64"]
     fn reduce_mul_i64x2(x: i64x2) -> i64;
-    #[link_name = "llvm.experimental.vector.reduce.mul.u64.u64x2"]
+    #[link_name = "llvm.experimental.vector.reduce.mul.u64.v2u64"]
     fn reduce_mul_u64x2(x: u64x2) -> u64;
-    #[link_name = "llvm.experimental.vector.reduce.mul.i8.i8x4"]
+    #[link_name = "llvm.experimental.vector.reduce.mul.i8.v4i8"]
     fn reduce_mul_i8x4(x: i8x4) -> i8;
-    #[link_name = "llvm.experimental.vector.reduce.mul.u8.u8x4"]
+    #[link_name = "llvm.experimental.vector.reduce.mul.u8.v4u8"]
     fn reduce_mul_u8x4(x: u8x4) -> u8;
-    #[link_name = "llvm.experimental.vector.reduce.mul.i16.i16x4"]
+    #[link_name = "llvm.experimental.vector.reduce.mul.i16.v4i16"]
     fn reduce_mul_i16x4(x: i16x4) -> i16;
-    #[link_name = "llvm.experimental.vector.reduce.mul.u16.u16x4"]
+    #[link_name = "llvm.experimental.vector.reduce.mul.u16.v4u16"]
     fn reduce_mul_u16x4(x: u16x4) -> u16;
-    #[link_name = "llvm.experimental.vector.reduce.mul.i32.i32x4"]
+    #[link_name = "llvm.experimental.vector.reduce.mul.i32.v4i32"]
     fn reduce_mul_i32x4(x: i32x4) -> i32;
-    #[link_name = "llvm.experimental.vector.reduce.mul.u32.u32x4"]
+    #[link_name = "llvm.experimental.vector.reduce.mul.u32.v4u32"]
     fn reduce_mul_u32x4(x: u32x4) -> u32;
-    #[link_name = "llvm.experimental.vector.reduce.mul.i64.i64x4"]
+    #[link_name = "llvm.experimental.vector.reduce.mul.i64.v4i64"]
     fn reduce_mul_i64x4(x: i64x4) -> i64;
-    #[link_name = "llvm.experimental.vector.reduce.mul.u64.u64x4"]
+    #[link_name = "llvm.experimental.vector.reduce.mul.u64.v4u64"]
     fn reduce_mul_u64x4(x: u64x4) -> u64;
-    #[link_name = "llvm.experimental.vector.reduce.mul.i8.i8x8"]
+    #[link_name = "llvm.experimental.vector.reduce.mul.i8.v8i8"]
     fn reduce_mul_i8x8(x: i8x8) -> i8;
-    #[link_name = "llvm.experimental.vector.reduce.mul.u8.u8x8"]
+    #[link_name = "llvm.experimental.vector.reduce.mul.u8.v8u8"]
     fn reduce_mul_u8x8(x: u8x8) -> u8;
-    #[link_name = "llvm.experimental.vector.reduce.mul.i16.i16x8"]
+    #[link_name = "llvm.experimental.vector.reduce.mul.i16.v8i16"]
     fn reduce_mul_i16x8(x: i16x8) -> i16;
-    #[link_name = "llvm.experimental.vector.reduce.mul.u16.u16x8"]
+    #[link_name = "llvm.experimental.vector.reduce.mul.u16.v8u16"]
     fn reduce_mul_u16x8(x: u16x8) -> u16;
-    #[link_name = "llvm.experimental.vector.reduce.mul.i32.i32x8"]
+    #[link_name = "llvm.experimental.vector.reduce.mul.i32.v8i32"]
     fn reduce_mul_i32x8(x: i32x8) -> i32;
-    #[link_name = "llvm.experimental.vector.reduce.mul.u32.u32x8"]
+    #[link_name = "llvm.experimental.vector.reduce.mul.u32.v8u32"]
     fn reduce_mul_u32x8(x: u32x8) -> u32;
-    #[link_name = "llvm.experimental.vector.reduce.mul.i64.i64x8"]
+    #[link_name = "llvm.experimental.vector.reduce.mul.i64.v8i64"]
     fn reduce_mul_i64x8(x: i64x8) -> i64;
-    #[link_name = "llvm.experimental.vector.reduce.mul.u64.u64x8"]
+    #[link_name = "llvm.experimental.vector.reduce.mul.u64.v8u64"]
     fn reduce_mul_u64x8(x: u64x8) -> u64;
-    #[link_name = "llvm.experimental.vector.reduce.mul.i8.i8x16"]
+    #[link_name = "llvm.experimental.vector.reduce.mul.i8.v16i8"]
     fn reduce_mul_i8x16(x: i8x16) -> i8;
-    #[link_name = "llvm.experimental.vector.reduce.mul.u8.u8x16"]
+    #[link_name = "llvm.experimental.vector.reduce.mul.u8.v16u8"]
     fn reduce_mul_u8x16(x: u8x16) -> u8;
-    #[link_name = "llvm.experimental.vector.reduce.mul.i16.i16x16"]
+    #[link_name = "llvm.experimental.vector.reduce.mul.i16.v16i16"]
     fn reduce_mul_i16x16(x: i16x16) -> i16;
-    #[link_name = "llvm.experimental.vector.reduce.mul.u16.u16x16"]
+    #[link_name = "llvm.experimental.vector.reduce.mul.u16.v16u16"]
     fn reduce_mul_u16x16(x: u16x16) -> u16;
-    #[link_name = "llvm.experimental.vector.reduce.mul.i32.i32x16"]
+    #[link_name = "llvm.experimental.vector.reduce.mul.i32.v16i32"]
     fn reduce_mul_i32x16(x: i32x16) -> i32;
-    #[link_name = "llvm.experimental.vector.reduce.mul.u32.u32x16"]
+    #[link_name = "llvm.experimental.vector.reduce.mul.u32.v16u32"]
     fn reduce_mul_u32x16(x: u32x16) -> u32;
-    #[link_name = "llvm.experimental.vector.reduce.mul.i8.i8x32"]
+    #[link_name = "llvm.experimental.vector.reduce.mul.i8.v32i8"]
     fn reduce_mul_i8x32(x: i8x32) -> i8;
-    #[link_name = "llvm.experimental.vector.reduce.mul.u8.u8x32"]
+    #[link_name = "llvm.experimental.vector.reduce.mul.u8.v32u8"]
     fn reduce_mul_u8x32(x: u8x32) -> u8;
-    #[link_name = "llvm.experimental.vector.reduce.mul.i16.i16x32"]
+    #[link_name = "llvm.experimental.vector.reduce.mul.i16.v32i16"]
     fn reduce_mul_i16x32(x: i16x32) -> i16;
-    #[link_name = "llvm.experimental.vector.reduce.mul.u16.u16x32"]
+    #[link_name = "llvm.experimental.vector.reduce.mul.u16.v32u16"]
     fn reduce_mul_u16x32(x: u16x32) -> u16;
-    #[link_name = "llvm.experimental.vector.reduce.mul.i8.i8x64"]
+    #[link_name = "llvm.experimental.vector.reduce.mul.i8.v64i8"]
     fn reduce_mul_i8x64(x: i8x64) -> i8;
-    #[link_name = "llvm.experimental.vector.reduce.mul.u8.u8x64"]
+    #[link_name = "llvm.experimental.vector.reduce.mul.u8.v64u8"]
     fn reduce_mul_u8x64(x: u8x64) -> u8;
-    /*
-    #[link_name = "llvm.experimental.vector.reduce.fmul.f32.f32x2"]
+    #[link_name = "llvm.experimental.vector.reduce.fmul.f32.v2f32"]
     fn reduce_fmul_f32x2(acc: f32, x: f32x2) -> f32;
-    #[link_name = "llvm.experimental.vector.reduce.fmul.f64.f64x2"]
+    #[link_name = "llvm.experimental.vector.reduce.fmul.f64.v2f64"]
     fn reduce_fmul_f64x2(acc: f64, x: f64x2) -> f64;
-    #[link_name = "llvm.experimental.vector.reduce.fmul.f32.f32x4"]
+    #[link_name = "llvm.experimental.vector.reduce.fmul.f32.v4f32"]
     fn reduce_fmul_f32x4(acc: f32, x: f32x4) -> f32;
-    #[link_name = "llvm.experimental.vector.reduce.fmul.f64.f64x4"]
+    #[link_name = "llvm.experimental.vector.reduce.fmul.f64.v4f64"]
     fn reduce_fmul_f64x4(acc: f64, x: f64x4) -> f64;
-    #[link_name = "llvm.experimental.vector.reduce.fmul.f32.f32x8"]
+    #[link_name = "llvm.experimental.vector.reduce.fmul.f32.v8f32"]
     fn reduce_fmul_f32x8(acc: f32, x: f32x8) -> f32;
-    #[link_name = "llvm.experimental.vector.reduce.fmul.f64.f64x8"]
+    #[link_name = "llvm.experimental.vector.reduce.fmul.f64.v8f64"]
     fn reduce_fmul_f64x8(acc: f64, x: f64x8) -> f64;
-    #[link_name = "llvm.experimental.vector.reduce.fmul.f32.f32x16"]
+    #[link_name = "llvm.experimental.vector.reduce.fmul.f32.v16f32"]
     fn reduce_fmul_f32x16(acc: f32, x: f32x16) -> f32;
-    */
 }
 
 /// Reduction: horizontal product of the vector elements.
