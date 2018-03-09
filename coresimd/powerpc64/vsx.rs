6f9aa02cfb13f5a8b1a2dc52195f388f70c37997
gnzlbg <gonzalobg88@gmail.com>
2018-03-09 17:38:09 +0100

    add some powerpc/powerpc64 altivec/vsx intrinsics

---
+++ coresimd/powerpc64/vsx.rs
@@ -0,0 +1,23 @@
+//! PowerPC Vectir Scalar eXtensions (VSX) intrinsics.
+//!
+//! The references are: [POWER ISA v2.07B (for POWER8 & POWER8 with NVIDIA
+//! NVlink)] and [POWER ISA v3.0B (for POWER9)].
+//!
+//! [POWER ISA v2.07B (for POWER8 & POWER8 with NVIDIA NVlink)]: https://ibm.box.com/s/jd5w15gz301s5b5dt375mshpq9c3lh4u
+//! [POWER ISA v3.0B (for POWER9)]: https://ibm.box.com/s/1hzcwkwf8rbju5h9iyf44wm94amnlcrv
+
+#[cfg(test)]
+use stdsimd_test::assert_instr;
+use coresimd::simd_llvm::simd_add;
+use coresimd::simd::*;
+
+// pub type vector_Float16 = f16x8;
+pub type vector_signed_long = i64x2;
+pub type vector_unsigned_long = u64x2;
+pub type vector_bool_long = u64x2;
+pub type vector_signed_long_long = vector_signed_long;
+pub type vector_unsigned_long_long = vector_unsigned_long;
+pub type vector_bool_long_long = vector_bool_long;
+pub type vector_double = f64x2;
+// pub type vector_signed___int128 = i128x1;
+// pub type vector_unsigned___int128 = i128x1;
