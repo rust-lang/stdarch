4c55f966397b0949d37b43550d707d8278175544
gnzlbg <gonzalobg88@gmail.com>
2018-03-20 23:18:35 +0100

    documents arithmetic reduction semantics

--- coresimd/ppsv/api/minmax_reductions.rs
+++ coresimd/ppsv/api/minmax_reductions.rs
@@ -4,22 +4,19 @@
 macro_rules! impl_minmax_reductions {
     ($id:ident, $elem_ty:ident) => {
         impl $id {
-            /// Largest vector value.
-            ///
-            /// FIXME: document behavior for float vectors with NaNs.
+            /// Largest vector element value.
             #[cfg(not(target_arch = "aarch64"))]
             #[inline]
-            pub fn max(self) -> $elem_ty {
+            pub fn max_element(self) -> $elem_ty {
                 use coresimd::simd_llvm::simd_reduce_max;
                 unsafe { simd_reduce_max(self) }
             }
-            /// Largest vector value.
-            ///
-            /// FIXME: document behavior for float vectors with NaNs.
+
+            /// Largest vector element value.
             #[cfg(target_arch = "aarch64")]
             #[allow(unused_imports)]
             #[inline]
-            pub fn max(self) -> $elem_ty {
+            pub fn max_element(self) -> $elem_ty {
                 // FIXME: broken on AArch64
                 // https://bugs.llvm.org/show_bug.cgi?id=36796
                 use cmp::Ord;
@@ -31,22 +28,19 @@ macro_rules! impl_minmax_reductions {
                 x
             }
 
-            /// Smallest vector value.
-            ///
-            /// FIXME: document behavior for float vectors with NaNs.
+            /// Smallest vector element value.
             #[cfg(not(target_arch = "aarch64"))]
             #[inline]
-            pub fn min(self) -> $elem_ty {
+            pub fn min_element(self) -> $elem_ty {
                 use coresimd::simd_llvm::simd_reduce_min;
                 unsafe { simd_reduce_min(self) }
             }
-            /// Smallest vector value.
-            ///
-            /// FIXME: document behavior for float vectors with NaNs.
+
+            /// Smallest vector element value.
             #[cfg(target_arch = "aarch64")]
             #[allow(unused_imports)]
             #[inline]
-            pub fn min(self) -> $elem_ty {
+            pub fn min_element(self) -> $elem_ty {
                 // FIXME: broken on AArch64
                 // https://bugs.llvm.org/show_bug.cgi?id=36796
                 use cmp::Ord;
@@ -65,29 +59,29 @@ macro_rules! impl_minmax_reductions {
 macro_rules! test_minmax_reductions {
     ($id:ident, $elem_ty:ident) => {
         #[test]
-        fn max() {
+        fn max_element() {
             use coresimd::simd::$id;
             let v = $id::splat(0 as $elem_ty);
-            assert_eq!(v.max(), 0 as $elem_ty);
+            assert_eq!(v.max_element(), 0 as $elem_ty);
             let v = v.replace(1, 1 as $elem_ty);
-            assert_eq!(v.max(), 1 as $elem_ty);
+            assert_eq!(v.max_element(), 1 as $elem_ty);
             let v = v.replace(0, 2 as $elem_ty);
-            assert_eq!(v.max(), 2 as $elem_ty);
+            assert_eq!(v.max_element(), 2 as $elem_ty);
         }
 
         #[test]
-        fn min() {
+        fn min_element() {
             use coresimd::simd::$id;
             let v = $id::splat(0 as $elem_ty);
-            assert_eq!(v.min(), 0 as $elem_ty);
+            assert_eq!(v.min_element(), 0 as $elem_ty);
             let v = v.replace(1, 1 as $elem_ty);
-            assert_eq!(v.min(), 0 as $elem_ty);
+            assert_eq!(v.min_element(), 0 as $elem_ty);
             let v = $id::splat(1 as $elem_ty);
             let v = v.replace(0, 2 as $elem_ty);
-            assert_eq!(v.min(), 1 as $elem_ty);
+            assert_eq!(v.min_element(), 1 as $elem_ty);
             let v = $id::splat(2 as $elem_ty);
             let v = v.replace(1, 1 as $elem_ty);
-            assert_eq!(v.min(), 1 as $elem_ty);
+            assert_eq!(v.min_element(), 1 as $elem_ty);
         }
     };
 }
