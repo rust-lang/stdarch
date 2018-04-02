929991752192528e4f9e827688cb4811d52d42c6
Adam Niederer <adam.niederer@gmail.com>
2018-04-02 12:58:28 -0400

    Remove i128 feature

--- coresimd/src/lib.rs
+++ coresimd/src/lib.rs
@@ -12,7 +12,7 @@
 #![allow(dead_code)]
 #![allow(unused_features)]
 #![feature(const_fn, link_llvm_intrinsics, platform_intrinsics, repr_simd,
-           simd_ffi, target_feature, cfg_target_feature, i128_type, asm,
+           simd_ffi, target_feature, cfg_target_feature, asm,
            const_atomic_usize_new, stmt_expr_attributes, core_intrinsics,
            crate_in_paths)]
 #![cfg_attr(test, feature(proc_macro, test, repr_align, attr_literals))]
