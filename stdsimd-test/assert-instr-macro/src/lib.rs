d6874383653c8b93832d53092078a41b146be941
Adam Niederer <adam.niederer@gmail.com>
2018-04-02 13:27:09 -0400

    Update assert_instr
    Brings it in line with the latest master

--- stdsimd-test/assert-instr-macro/src/lib.rs
+++ stdsimd-test/assert-instr-macro/src/lib.rs
@@ -10,8 +10,8 @@
 
 #![feature(proc_macro)]
 
-extern crate proc_macro2;
 extern crate proc_macro;
+extern crate proc_macro2;
 #[macro_use]
 extern crate quote;
 #[macro_use]
@@ -39,59 +39,71 @@ pub fn assert_instr(
         (quote! { #[ignore] }).into()
     };
     let name = &func.ident;
+    use quote::ToTokens;
+    let instr_str = instr
+        .clone()
+        .into_tokens()
+        .to_string()
+        .replace('.', "_")
+        .replace(|c: char| c.is_whitespace(), "");
     let assert_name = syn::Ident::from(
-        &format!("assert_{}_{}", name.as_ref(), instr.as_ref())[..],
+        &format!("assert_{}_{}", name.as_ref(), instr_str)[..],
     );
     let shim_name = syn::Ident::from(format!("{}_shim", name.as_ref()));
-    let (to_test, test_name) = if invoc.args.len() == 0 {
-        (TokenStream::empty(), &func.ident)
-    } else {
-        let mut inputs = Vec::new();
-        let mut input_vals = Vec::new();
-        let ret = &func.decl.output;
-        for arg in func.decl.inputs.iter() {
-            let capture = match *arg {
-                syn::FnArg::Captured(ref c) => c,
-                _ => panic!("arguments must not have patterns"),
-            };
-            let ident = match capture.pat {
-                syn::Pat::Ident(ref i) => &i.ident,
-                _ => panic!("must have bare arguments"),
-            };
-            match invoc.args.iter().find(|a| a.0 == ident.as_ref()) {
-                Some(&(_, ref tts)) => {
-                    input_vals.push(quote! { #tts });
-                }
-                None => {
-                    inputs.push(capture);
-                    input_vals.push(quote! { #ident });
-                }
-            };
-        }
-
-        let attrs = func.attrs
+    let mut inputs = Vec::new();
+    let mut input_vals = Vec::new();
+    let ret = &func.decl.output;
+    for arg in func.decl.inputs.iter() {
+        let capture = match *arg {
+            syn::FnArg::Captured(ref c) => c,
+            _ => panic!("arguments must not have patterns"),
+        };
+        let ident = match capture.pat {
+            syn::Pat::Ident(ref i) => &i.ident,
+            _ => panic!("must have bare arguments"),
+        };
+        match invoc
+            .args
             .iter()
-            .filter(|attr| {
-                attr.path
-                    .segments
-                    .first()
-                    .unwrap()
-                    .value()
-                    .ident
-                    .as_ref()
-                    .starts_with("target")
-            })
-            .collect::<Vec<_>>();
-        let attrs = Append(&attrs);
-        (
-            quote! {
-                #attrs
-                unsafe fn #shim_name(#(#inputs),*) #ret {
-                    #name(#(#input_vals),*)
-                }
-            }.into(),
-            &shim_name,
-        )
+            .find(|a| a.0 == ident.as_ref())
+        {
+            Some(&(_, ref tts)) => {
+                input_vals.push(quote! { #tts });
+            }
+            None => {
+                inputs.push(capture);
+                input_vals.push(quote! { #ident });
+            }
+        };
+    }
+
+    let attrs = func.attrs
+        .iter()
+        .filter(|attr| {
+            attr.path
+                .segments
+                .first()
+                .expect("attr.path.segments.first() failed")
+                .value()
+                .ident
+                .as_ref()
+                .starts_with("target")
+        })
+        .collect::<Vec<_>>();
+    let attrs = Append(&attrs);
+
+    // Use an ABI on Windows that passes SIMD values in registers, like what
+    // happens on Unix (I think?) by default.
+    let abi = if cfg!(windows) {
+        syn::LitStr::new("vectorcall", proc_macro2::Span::call_site())
+    } else {
+        syn::LitStr::new("C", proc_macro2::Span::call_site())
+    };
+    let to_test = quote! {
+        #attrs
+        unsafe extern #abi fn #shim_name(#(#inputs),*) #ret {
+            #name(#(#input_vals),*)
+        }
     };
 
     let tts: TokenStream = quote_spanned! {
@@ -102,13 +114,15 @@ pub fn assert_instr(
         fn #assert_name() {
             #to_test
 
-            ::stdsimd_test::assert(#test_name as usize,
-                                   stringify!(#test_name),
+            ::stdsimd_test::assert(#shim_name as usize,
+                                   stringify!(#shim_name),
                                    stringify!(#instr));
         }
     }.into();
     // why? necessary now to get tests to work?
-    let tts: TokenStream = tts.to_string().parse().unwrap();
+    let tts: TokenStream = tts.to_string()
+        .parse()
+        .expect("cannot parse tokenstream");
 
     let tts: TokenStream = quote! {
         #item
@@ -118,13 +132,13 @@ pub fn assert_instr(
 }
 
 struct Invoc {
-    instr: syn::Ident,
+    instr: syn::Expr,
     args: Vec<(syn::Ident, syn::Expr)>,
 }
 
 impl syn::synom::Synom for Invoc {
     named!(parse -> Self, map!(parens!(do_parse!(
-        instr: syn!(syn::Ident) >>
+        instr: syn!(syn::Expr) >>
         args: many0!(do_parse!(
             syn!(syn::token::Comma) >>
             name: syn!(syn::Ident) >>
