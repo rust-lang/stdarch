86b1f480d4bffd7a063a62b56e09ef880d8573f9
TheIronBorn <>
2018-06-29 16:13:03 -0700

    try instr prefix

--- crates/coresimd/tests/rotate_tests.rs
+++ crates/coresimd/tests/rotate_tests.rs
@@ -15,35 +15,35 @@ use stdsimd_test::assert_instr;
 
 #[inline]
 #[cfg_attr(any(target_arch = "x86", target_arch = "x86_64"), target_feature(enable = "avx512f"))]
-#[cfg_attr(any(target_arch = "x86", target_arch = "x86_64"), assert_instr(vprorvq))]
+#[cfg_attr(any(target_arch = "x86", target_arch = "x86_64"), assert_instr(vpro))]
 unsafe fn rotate_right_variable(x: u64x8) -> u64x8 {
     x.rotate_right(u64x8::new(0, 1, 2, 3, 4, 5, 6, 7))
 }
 
 #[inline]
 #[cfg_attr(any(target_arch = "x86", target_arch = "x86_64"), target_feature(enable = "avx512f"))]
-#[cfg_attr(any(target_arch = "x86", target_arch = "x86_64"), assert_instr(vprolvq))]
+#[cfg_attr(any(target_arch = "x86", target_arch = "x86_64"), assert_instr(vpro))]
 unsafe fn rotate_left_variable(x: u64x8) -> u64x8 {
     x.rotate_left(u64x8::new(0, 1, 2, 3, 4, 5, 6, 7))
 }
 
 #[inline]
 #[cfg_attr(any(target_arch = "x86", target_arch = "x86_64"), target_feature(enable = "avx512f"))]
-#[cfg_attr(any(target_arch = "x86", target_arch = "x86_64"), assert_instr(vprorq))]
+#[cfg_attr(any(target_arch = "x86", target_arch = "x86_64"), assert_instr(vpro))]
 unsafe fn rotate_right(x: u64x8) -> u64x8 {
     x.rotate_right(u64x8::splat(12))
 }
 
 #[inline]
 #[cfg_attr(any(target_arch = "x86", target_arch = "x86_64"), target_feature(enable = "avx512f"))]
-#[cfg_attr(any(target_arch = "x86", target_arch = "x86_64"), assert_instr(vprolq))]
+#[cfg_attr(any(target_arch = "x86", target_arch = "x86_64"), assert_instr(vpro))]
 unsafe fn rotate_left(x: u64x8) -> u64x8 {
     x.rotate_left(u64x8::splat(12))
 }
 
 #[inline]
 #[cfg_attr(any(target_arch = "x86", target_arch = "x86_64"), target_feature(enable = "avx512f"))]
-#[cfg_attr(any(target_arch = "x86", target_arch = "x86_64"), assert_instr(vprolq))]
+#[cfg_attr(any(target_arch = "x86", target_arch = "x86_64"), assert_instr(vpro))]
 unsafe fn rotate_left_x2(x: u64x2) -> u64x2 {
     x.rotate_left(u64x2::splat(12))
 }
