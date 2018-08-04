4636e0f02d5f5f0a09393e98f03db3b428237bd1
Seiichi Uchida <seuchida@gmail.com>
2018-08-04 19:05:37 +0900

    Add `#` prefix to doc comment
    This will fix broken rustfmt in CI.

--- stdsimd/mod.rs
+++ stdsimd/mod.rs
@@ -59,7 +59,7 @@
 ///
 /// ```ignore
 /// #[cfg(all(any(target_arch = "x86", target_arch = "x86_64"),
-///       target_feature = "avx2"))]
+/// #     target_feature = "avx2"))]
 /// fn foo() {
 ///     #[cfg(target_arch = "x86")]
 ///     use std::arch::x86::_mm256_add_epi64;
