d6c2354b1c2d869dae6b6612623910eb0615987b
Daniel Smith <daniel.smith@datadoghq.com>
2020-06-07 18:32:01 +0000

    Delete mistaken comment

--- crates/core_arch/src/x86_64/avx512f.rs
+++ crates/core_arch/src/x86_64/avx512f.rs
@@ -637,33 +637,4 @@ mod tests {
         }
         assert_eq!(&arr[..], &expected[..],);
     }
-
-    /*
-    #[simd_test(enable = "avx512f")]
-    unsafe fn test_mm512_i64gather_epi32() {
-        let mut arr = [0i64; 128];
-        for i in 0..128i64 {
-            arr[i as usize] = i;
-        }
-        // A multiplier of 8 is word-addressing
-        #[rustfmt::skip]
-        let index = _mm512_setr_epi64(0, 16, 32, 48, 64, 80, 96, 112);
-        let r = _mm512_i64gather_epi32(index, arr.as_ptr() as *const u8, 8);
-        assert_eq_m256i(r, _mm256_setr_epi32(0, 16, 32, 48, 64, 80, 96, 112));
-    }
-
-    #[simd_test(enable = "avx512f")]
-    unsafe fn test_mm512_mask_i64gather_epi32() {
-        let mut arr = [0i64; 128];
-        for i in 0..128i64 {
-            arr[i as usize] = i;
-        }
-        let src = _mm256_set1_epi32(2);
-        let mask = 0b10101010;
-        #[rustfmt::skip]
-        let index = _mm512_setr_epi64(0, 16, 32, 48, 64, 80, 96, 112);
-        // A multiplier of 8 is word-addressing
-        let r = _mm512_mask_i64gather_epi32(src, mask, index, arr.as_ptr() as *const u8, 8);
-        assert_eq_m256i(r, _mm256_setr_epi32(2, 16, 2, 48, 2, 80, 2, 112));
-    }*/
 }
