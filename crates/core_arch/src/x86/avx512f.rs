a5f32358ada608c50ec2be54470ec973cd4b7ffa
jironglin <jironglin@gmail.com>
2020-09-19 17:35:16 +0000

    use simple bitwise operations on masks

--- crates/core_arch/src/x86/avx512f.rs
+++ crates/core_arch/src/x86/avx512f.rs
@@ -10466,7 +10466,7 @@ pub unsafe fn _mm512_xor_si512(a: __m512i, b: __m512i) -> __m512i {
 #[target_feature(enable = "avx512f")]
 #[cfg_attr(test, assert_instr(and))] // generate normal and code instead of kandw
 pub unsafe fn _kand_mask16(a: __mmask16, b: __mmask16) -> __mmask16 {
-    transmute(kandw(a, b))
+    transmute(a & b)
 }
 
 /// Compute the bitwise AND of 16-bit masks a and b, and store the result in k.
@@ -10476,7 +10476,7 @@ pub unsafe fn _kand_mask16(a: __mmask16, b: __mmask16) -> __mmask16 {
 #[target_feature(enable = "avx512f")]
 #[cfg_attr(test, assert_instr(and))] // generate normal and code instead of kandw
 pub unsafe fn _mm512_kand(a: __mmask16, b: __mmask16) -> __mmask16 {
-    transmute(kandw(a, b))
+    transmute(a & b)
 }
 
 /// Compute the bitwise OR of 16-bit masks a and b, and store the result in k.
@@ -10486,7 +10486,7 @@ pub unsafe fn _mm512_kand(a: __mmask16, b: __mmask16) -> __mmask16 {
 #[target_feature(enable = "avx512f")]
 #[cfg_attr(test, assert_instr(or))] // generate normal or code instead of korw
 pub unsafe fn _kor_mask16(a: __mmask16, b: __mmask16) -> __mmask16 {
-    transmute(korw(a, b))
+    transmute(a | b)
 }
 
 /// Compute the bitwise OR of 16-bit masks a and b, and store the result in k.
@@ -10496,7 +10496,7 @@ pub unsafe fn _kor_mask16(a: __mmask16, b: __mmask16) -> __mmask16 {
 #[target_feature(enable = "avx512f")]
 #[cfg_attr(test, assert_instr(or))] // generate normal or code instead of korw
 pub unsafe fn _mm512_kor(a: __mmask16, b: __mmask16) -> __mmask16 {
-    transmute(korw(a, b))
+    transmute(a | b)
 }
 
 /// Compute the bitwise XOR of 16-bit masks a and b, and store the result in k.
@@ -10506,7 +10506,7 @@ pub unsafe fn _mm512_kor(a: __mmask16, b: __mmask16) -> __mmask16 {
 #[target_feature(enable = "avx512f")]
 #[cfg_attr(test, assert_instr(xor))] // generate normal xor code instead of kxorw
 pub unsafe fn _kxor_mask16(a: __mmask16, b: __mmask16) -> __mmask16 {
-    transmute(kxorw(a, b))
+    transmute(a ^ b)
 }
 
 /// Compute the bitwise XOR of 16-bit masks a and b, and store the result in k.
@@ -10516,7 +10516,7 @@ pub unsafe fn _kxor_mask16(a: __mmask16, b: __mmask16) -> __mmask16 {
 #[target_feature(enable = "avx512f")]
 #[cfg_attr(test, assert_instr(xor))] // generate normal xor code instead of kxorw
 pub unsafe fn _mm512_kxor(a: __mmask16, b: __mmask16) -> __mmask16 {
-    transmute(kxorw(a, b))
+    transmute(a ^ b)
 }
 
 /// Compute the bitwise NOT of 16-bit mask a, and store the result in k.
@@ -10524,7 +10524,7 @@ pub unsafe fn _mm512_kxor(a: __mmask16, b: __mmask16) -> __mmask16 {
 /// [Intel's documentation](https://software.intel.com/sites/landingpage/IntrinsicsGuide/#text=knot_mask16&expand=3233)
 #[inline]
 #[target_feature(enable = "avx512f")]
-#[cfg_attr(all(test, not(target_os = "macos")), assert_instr(not))] // generate normal not code instead of knotw
+#[cfg_attr(test, assert_instr(not))] // generate normal not code instead of knotw
 pub unsafe fn _knot_mask16(a: __mmask16) -> __mmask16 {
     transmute(_mm512_kxor(a, 0b11111111_11111111))
 }
@@ -10534,7 +10534,7 @@ pub unsafe fn _knot_mask16(a: __mmask16) -> __mmask16 {
 /// [Intel's documentation](https://software.intel.com/sites/landingpage/IntrinsicsGuide/#text=512_knot&expand=3231)
 #[inline]
 #[target_feature(enable = "avx512f")]
-#[cfg_attr(all(test, not(target_os = "macos")), assert_instr(not))] // generate normal not code instead of knotw
+#[cfg_attr(test, assert_instr(not))] // generate normal not code instead of knotw
 pub unsafe fn _mm512_knot(a: __mmask16) -> __mmask16 {
     transmute(_mm512_kxor(a, 0b11111111_11111111))
 }
