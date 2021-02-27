caba6f4667bbabbf50304d61a33295c24ca3ba74
Tomasz Miąsko <tomasz.miasko@gmail.com>
2021-02-27 00:00:00 +0000

    Convert `_mm_srli_epi32` to const generics

--- crates/core_arch/src/x86/sse2.rs
+++ crates/core_arch/src/x86/sse2.rs
@@ -738,16 +738,10 @@ pub unsafe fn _mm_srl_epi16(a: __m128i, count: __m128i) -> __m128i {
 #[inline]
 #[target_feature(enable = "sse2")]
 #[cfg_attr(test, assert_instr(psrld, imm8 = 8))]
-#[rustc_args_required_const(1)]
+#[rustc_legacy_const_generics(1)]
 #[stable(feature = "simd_x86", since = "1.27.0")]
-pub unsafe fn _mm_srli_epi32(a: __m128i, imm8: i32) -> __m128i {
-    let a = a.as_i32x4();
-    macro_rules! call {
-        ($imm8:expr) => {
-            transmute(psrlid(a, $imm8))
-        };
-    }
-    constify_imm8!(imm8, call)
+pub unsafe fn _mm_srli_epi32<const imm8: i32>(a: __m128i) -> __m128i {
+    transmute(psrlid(a.as_i32x4(), imm8))
 }
 
 /// Shifts packed 32-bit integers in `a` right by `count` while shifting in
@@ -3576,7 +3570,7 @@ mod tests {
 
     #[simd_test(enable = "sse2")]
     unsafe fn test_mm_srli_epi32() {
-        let r = _mm_srli_epi32(_mm_set1_epi32(0xFFFF), 4);
+        let r = _mm_srli_epi32::<4>(_mm_set1_epi32(0xFFFF));
         assert_eq_m128i(r, _mm_set1_epi32(0xFFF));
     }
 
