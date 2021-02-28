14ab01456bb0aed1a1d88d431095573762a8f199
Lzu Tao <taolzu@gmail.com>
2021-02-28 12:47:11 +0700

    Add static_assert_imm_s{5,11,12,13} macros

--- crates/core_arch/src/mips/msa/macros.rs
+++ crates/core_arch/src/mips/msa/macros.rs
@@ -1,8 +1,32 @@
 //! Utility macros.
 
+macro_rules! static_assert_imm_s5 {
+    ($imm:ident) => {
+        let _ = $crate::core_arch::macros::ValidateConstImm::<$imm, -16, 15>::VALID;
+    };
+}
+
 macro_rules! static_assert_imm_s10 {
     ($imm:ident) => {
         let _ = $crate::core_arch::macros::ValidateConstImm::<$imm, -512, 511>::VALID;
+    };
+}
+
+macro_rules! static_assert_imm_s11 {
+    ($imm:ident) => {
+        let _ = $crate::core_arch::macros::ValidateConstImm::<$imm, -1024, 1022>::VALID;
+    };
+}
+
+macro_rules! static_assert_imm_s12 {
+    ($imm:ident) => {
+        let _ = $crate::core_arch::macros::ValidateConstImm::<$imm, -2048, 2044>::VALID;
+    };
+}
+
+macro_rules! static_assert_imm_s13 {
+    ($imm:ident) => {
+        let _ = $crate::core_arch::macros::ValidateConstImm::<$imm, -4096, 4088>::VALID;
     };
 }
 
