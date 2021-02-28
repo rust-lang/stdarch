59df62d6ca19dc33c47f9d2095cb5a87616c0260
Rémy Rakic <remy.rakic+github@gmail.com>
2021-02-28 15:56:18 +0100

    convert `_mm_cmpistrz` to const generics

--- crates/core_arch/src/x86/sse42.rs
+++ crates/core_arch/src/x86/sse42.rs
@@ -286,17 +286,11 @@ pub unsafe fn _mm_cmpistri(a: __m128i, b: __m128i, imm8: i32) -> i32 {
 #[inline]
 #[target_feature(enable = "sse4.2")]
 #[cfg_attr(test, assert_instr(pcmpistri, imm8 = 0))]
-#[rustc_args_required_const(2)]
+#[rustc_legacy_const_generics(2)]
 #[stable(feature = "simd_x86", since = "1.27.0")]
-pub unsafe fn _mm_cmpistrz(a: __m128i, b: __m128i, imm8: i32) -> i32 {
-    let a = a.as_i8x16();
-    let b = b.as_i8x16();
-    macro_rules! call {
-        ($imm8:expr) => {
-            pcmpistriz128(a, b, $imm8)
-        };
-    }
-    constify_imm8!(imm8, call)
+pub unsafe fn _mm_cmpistrz<const imm8: i32>(a: __m128i, b: __m128i) -> i32 {
+    static_assert_imm8!(imm8);
+    pcmpistriz128(a.as_i8x16(), b.as_i8x16(), imm8 as i8)
 }
 
 /// Compares packed strings with implicit lengths in `a` and `b` using the
@@ -674,7 +668,7 @@ mod tests {
     unsafe fn test_mm_cmpistrz() {
         let a = str_to_m128i(b"");
         let b = str_to_m128i(b"Hello");
-        let i = _mm_cmpistrz(a, b, _SIDD_CMP_EQUAL_ORDERED);
+        let i = _mm_cmpistrz::<_SIDD_CMP_EQUAL_ORDERED>(a, b);
         assert_eq!(1, i);
     }
 
