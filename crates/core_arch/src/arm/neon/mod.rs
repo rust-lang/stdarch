480a2ca60fe0118bb9cc83637a32a08866644087
SparrowLii <liyuan179@huawei.com>
2021-03-11 17:56:18 +0800

    correct tests

--- crates/core_arch/src/arm/neon/mod.rs
+++ crates/core_arch/src/arm/neon/mod.rs
@@ -10207,35 +10207,35 @@ mod tests {
         assert_eq!(r, e);
     }
     #[simd_test(enable = "neon")]
-    unsafe fn test_vclsq_s8() {
+    unsafe fn test_vclzq_s8() {
         let a: i8x16 = i8x16::new(-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14);
         let e: i8x16 = i8x16::new(0, 8, 7, 6, 6, 5, 5, 5, 5, 4, 4, 4, 4, 4, 4, 4);
         let r: i8x16 = transmute(vclzq_s8(transmute(a)));
         assert_eq!(r, e);
     }
     #[simd_test(enable = "neon")]
-    unsafe fn test_vcls_s16() {
+    unsafe fn test_vclz_s16() {
         let a: i16x4 = i16x4::new(-1, 0, 1, 2);
         let e: i16x4 = i16x4::new(0, 16, 15, 14);
         let r: i16x4 = transmute(vclz_s16(transmute(a)));
         assert_eq!(r, e);
     }
     #[simd_test(enable = "neon")]
-    unsafe fn test_vclsq_s16() {
+    unsafe fn test_vclzq_s16() {
         let a: i16x8 = i16x8::new(-1, 0, 1, 2, 3, 4, 5, 6);
         let e: i16x8 = i16x8::new(0, 16, 15, 14, 14, 13, 13, 13);
         let r: i16x8 = transmute(vclzq_s16(transmute(a)));
         assert_eq!(r, e);
     }
     #[simd_test(enable = "neon")]
-    unsafe fn test_vcls_s32() {
+    unsafe fn test_vclz_s32() {
         let a: i32x2 = i32x2::new(-1, 0);
         let e: i32x2 = i32x2::new(0, 32);
         let r: i32x2 = transmute(vclz_s32(transmute(a)));
         assert_eq!(r, e);
     }
     #[simd_test(enable = "neon")]
-    unsafe fn test_vclsq_s32() {
+    unsafe fn test_vclzq_s32() {
         let a: i32x4 = i32x4::new(-1, 0, 1, 2);
         let e: i32x4 = i32x4::new(0, 32, 31, 30);
         let r: i32x4 = transmute(vclzq_s32(transmute(a)));
