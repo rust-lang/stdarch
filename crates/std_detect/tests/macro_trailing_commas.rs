rust-lang/stdarch

commit 68b3ddfff12d7ccbfa1aab295afc6f4969b3f050

Fix various compilation errors

crates/core_arch/src/lib.rs
@@ -74,6 +74,7 @@ mod core_arch;
 #[stable(feature = "stdsimd", since = "1.27.0")]
 pub mod arch {
     #[stable(feature = "stdsimd", since = "1.27.0")]
+    #[allow(unused_imports)]
     pub use crate::core_arch::arch::*;
     #[stable(feature = "stdsimd", since = "1.27.0")]
     pub use core::arch::asm;

crates/std_detect/src/lib.rs
@@ -21,6 +21,8 @@
 #![no_std]
 #![allow(internal_features)]
 // Temporary hack: needed to build against toolchains from before the mass feature renaming.
+// Remove this as soon as the stdarch submodule is updated on nightly.
+#![allow(stable_features)]
 #![feature(stdsimd)]
 
 #[cfg(test)]

examples/connect5.rs
@@ -28,7 +28,9 @@
 //! You should see a game self-playing. In the end of the game, it shows the average time for
 //! each move.
 
-#![feature(avx512_target_feature, stdarch_x86_avx512)]
+#![feature(avx512_target_feature)]
+#![cfg_attr(target_arch = "x86", feature(stdarch_x86_avx512))]
+#![cfg_attr(target_arch = "x86_64", feature(stdarch_x86_avx512))]
 #![feature(stmt_expr_attributes)]
 
 use rand::seq::SliceRandom;

crates/std_detect/tests/macro_trailing_commas.rs
@@ -1,4 +1,6 @@
-#![feature(stdarch_arm_feature_detection, stdarch_powerpc_feature_detection)]
+#![cfg_attr(target_arch = "arm", feature(stdarch_arm_feature_detection))]
+#![cfg_attr(target_arch = "powerpc", feature(stdarch_powerpc_feature_detection))]
+#![cfg_attr(target_arch = "powerpc64", feature(stdarch_powerpc_feature_detection))]
 #![allow(clippy::unwrap_used, clippy::use_debug, clippy::print_stdout)]
 
 #[cfg(any(

crates/core_arch/src/arm/mod.rs
@@ -11,6 +11,8 @@
 mod sat;
 
 #[cfg(any(target_feature = "v6", doc))]
+// Remove warning because this module is currently empty.
+#[allow(unused_imports)]
 pub use self::sat::*;
 
 // Supported arches: 5TE, 7E-M. See Section 10.1 of ACLE (e.g. QADD)

crates/intrinsic-test/src/main.rs
@@ -166,6 +166,14 @@ fn generate_rust_program(notices: &str, intrinsic: &Intrinsic, a32: bool) -> Str
     format!(
         r#"{notices}#![feature(simd_ffi)]
 #![feature(link_llvm_intrinsics)]
+#![cfg_attr(target_arch = "arm", feature(stdarch_arm_neon_intrinsics))]
+#![feature(stdarch_arm_crc32)]
+#![cfg_attr(target_arch = "aarch64", feature(stdarch_neon_fcma))]
+#![cfg_attr(target_arch = "aarch64", feature(stdarch_neon_dotprod))]
+#![cfg_attr(target_arch = "aarch64", feature(stdarch_neon_i8mm))]
+#![cfg_attr(target_arch = "aarch64", feature(stdarch_neon_sha3))]
+#![cfg_attr(target_arch = "aarch64", feature(stdarch_neon_sm4))]
+#![cfg_attr(target_arch = "aarch64", feature(stdarch_neon_ftts))]
 #![allow(overflowing_literals)]
 #![allow(non_upper_case_globals)]
 use core_arch::arch::{target_arch}::*;

crates/std_detect/tests/cpu-detection.rs
@@ -1,19 +1,20 @@
-#![feature(
-    stdarch_internal,
-    stdarch_arm_feature_detection,
-    stdarch_powerpc_feature_detection
-)]
+#![feature(stdarch_internal)]
+#![cfg_attr(target_arch = "arm", feature(stdarch_arm_feature_detection))]
+#![cfg_attr(target_arch = "powerpc", feature(stdarch_powerpc_feature_detection))]
+#![cfg_attr(target_arch = "powerpc64", feature(stdarch_powerpc_feature_detection))]
 #![allow(clippy::unwrap_used, clippy::use_debug, clippy::print_stdout)]
-#![cfg(any(
-    target_arch = "arm",
-    target_arch = "aarch64",
-    target_arch = "x86",
-    target_arch = "x86_64",
-    target_arch = "powerpc",
-    target_arch = "powerpc64"
-))]
 
-#[macro_use]
+#[cfg_attr(
+    any(
+        target_arch = "arm",
+        target_arch = "aarch64",
+        target_arch = "x86",
+        target_arch = "x86_64",
+        target_arch = "powerpc",
+        target_arch = "powerpc64"
+    ),
+    macro_use
+)]
 extern crate std_detect;
 
 #[test]

crates/stdarch-verify/tests/arm.rs
@@ -1,8 +1,9 @@
+#![allow(unused)]
+
 use std::collections::HashMap;
 
 use serde::Deserialize;
 
-#[allow(unused)]
 struct Function {
     name: &'static str,
     arguments: &'static [&'static Type],

commit 6fd7679daea36f6b607536381e0cf078f14e7560

Fix std_detect not being an unstable crate
More fallout from #1486

crates/core_arch/src/lib.rs
@@ -35,7 +35,7 @@
     inline_const,
     generic_arg_infer
 )]
-#![cfg_attr(test, feature(test, abi_vectorcall))]
+#![cfg_attr(test, feature(test, abi_vectorcall, stdarch_internal))]
 #![deny(clippy::missing_inline_in_public_items)]
 #![allow(
     clippy::identity_op,

crates/std_detect/src/lib.rs
@@ -13,7 +13,7 @@
 //! * `powerpc64`: [`is_powerpc64_feature_detected`]
 //! * `loongarch`: [`is_loongarch_feature_detected`]
 
-#![stable(feature = "stdsimd", since = "1.27.0")]
+#![unstable(feature = "stdarch_internal", issue = "none")]
 #![feature(staged_api, doc_cfg, allow_internal_unstable)]
 #![deny(rust_2018_idioms)]
 #![allow(clippy::shadow_reuse)]
@@ -23,7 +23,7 @@
 // Temporary hack: needed to build against toolchains from before the mass feature renaming.
 // Remove this as soon as the stdarch submodule is updated on nightly.
 #![allow(stable_features)]
-#![feature(stdsimd)]
+#![cfg_attr(not(feature = "rustc-dep-of-std"), feature(stdsimd))]
 #![cfg_attr(
     all(
         any(target_arch = "x86", target_arch = "x86_64"),
@@ -42,5 +42,5 @@ extern crate std;
 extern crate alloc;
 
 #[doc(hidden)]
-#[stable(feature = "stdsimd", since = "1.27.0")]
+#[unstable(feature = "stdarch_internal", issue = "none")]
 pub mod detect;

examples/connect5.rs
@@ -28,9 +28,10 @@
 //! You should see a game self-playing. In the end of the game, it shows the average time for
 //! each move.
 
+#![allow(internal_features)]
 #![feature(avx512_target_feature)]
-#![cfg_attr(target_arch = "x86", feature(stdarch_x86_avx512))]
-#![cfg_attr(target_arch = "x86_64", feature(stdarch_x86_avx512))]
+#![cfg_attr(target_arch = "x86", feature(stdarch_x86_avx512, stdarch_internal))]
+#![cfg_attr(target_arch = "x86_64", feature(stdarch_x86_avx512, stdarch_internal))]
 #![feature(stmt_expr_attributes)]
 
 use rand::seq::SliceRandom;

examples/hex.rs
@@ -12,8 +12,13 @@
 //!
 //! and you should see `746573740a` get printed out.
 
+#![allow(internal_features)]
 #![feature(wasm_target_feature)]
 #![cfg_attr(test, feature(test))]
+#![cfg_attr(
+    any(target_arch = "x86", target_arch = "x86_64"),
+    feature(stdarch_internal)
+)]
 #![allow(
     clippy::unwrap_used,
     clippy::print_stdout,

crates/std_detect/tests/macro_trailing_commas.rs
@@ -1,3 +1,15 @@
+#![allow(internal_features)]
+#![cfg_attr(
+    any(
+        target_arch = "arm",
+        target_arch = "aarch64",
+        target_arch = "x86",
+        target_arch = "x86_64",
+        target_arch = "powerpc",
+        target_arch = "powerpc64"
+    ),
+    feature(stdarch_internal)
+)]
 #![cfg_attr(target_arch = "arm", feature(stdarch_arm_feature_detection))]
 #![cfg_attr(target_arch = "powerpc", feature(stdarch_powerpc_feature_detection))]
 #![cfg_attr(target_arch = "powerpc64", feature(stdarch_powerpc_feature_detection))]

crates/std_detect/tests/x86-specific.rs
@@ -1,4 +1,6 @@
 #![cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+#![allow(internal_features)]
+#![feature(stdarch_internal)]
 
 extern crate cupid;
 #[macro_use]