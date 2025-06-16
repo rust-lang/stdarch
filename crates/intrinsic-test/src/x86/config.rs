16bd12cfd71e933cbc0e91b185dfed065c10bbaf
Madhav Madhusoodanan <madhavmadhusoodanan@gmail.com>
2025-06-16 12:56:33 +0530

    Add x86/config.rs to intrinsic-test

---
+++ crates/intrinsic-test/src/x86/config.rs
@@ -0,0 +1,9 @@
+pub fn build_notices(line_prefix: &str) -> String {
+    format!(
+        "\
+{line_prefix}This is a transient test file, not intended for distribution. Some aspects of the
+{line_prefix}test are derived from a JSON specification, published under the same license as the
+{line_prefix}`intrinsic-test` crate.\n
+"
+    )
+}
