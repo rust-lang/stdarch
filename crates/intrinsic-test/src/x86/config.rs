5b89089c9a19488b03a948c3d9b15c7e6d5ffe8f
Madhav Madhusoodanan <madhavmadhusoodanan@gmail.com>
2025-07-10 12:23:06 +0530

    feat: added initial functionality for building Rust files

--- crates/intrinsic-test/src/x86/config.rs
+++ crates/intrinsic-test/src/x86/config.rs
@@ -7,3 +7,35 @@ pub fn build_notices(line_prefix: &str) -> String {
 "
     )
 }
+
+// Format f16 values (and vectors containing them) in a way that is consistent with C.
+pub const F16_FORMATTING_DEF: &str = r#"
+#[repr(transparent)]
+struct Hex<T>(T);
+ "#;
+
+pub const X86_CONFIGURATIONS: &str = r#"
+#![cfg_attr(target_arch = "x86", feature(stdarch_x86_avx512))]
+#![cfg_attr(target_arch = "x86", feature(stdarch_x86_avx2))]
+#![cfg_attr(target_arch = "x86", feature(stdarch_x86_avx))]
+#![cfg_attr(target_arch = "x86", feature(stdarch_x86_sse42))]
+#![cfg_attr(target_arch = "x86", feature(stdarch_x86_sse41))]
+#![cfg_attr(target_arch = "x86", feature(stdarch_x86_ssse3))]
+#![cfg_attr(target_arch = "x86", feature(stdarch_x86_sse3))]
+#![cfg_attr(target_arch = "x86", feature(stdarch_x86_sse2))]
+#![cfg_attr(target_arch = "x86", feature(stdarch_x86_sse))]
+#![cfg_attr(target_arch = "x86_64", feature(stdarch_x86_avx512))]
+#![cfg_attr(target_arch = "x86_64", feature(stdarch_x86_avx2))]
+#![cfg_attr(target_arch = "x86_64", feature(stdarch_x86_avx))]
+#![cfg_attr(target_arch = "x86_64", feature(stdarch_x86_fma))]
+#![cfg_attr(target_arch = "x86_64", feature(stdarch_x86_aes))]
+#![cfg_attr(target_arch = "x86_64", feature(stdarch_x86_sha))]
+#![cfg_attr(target_arch = "x86_64", feature(stdarch_x86_bmi1))]
+#![cfg_attr(target_arch = "x86_64", feature(stdarch_x86_bmi2))]
+#![cfg_attr(target_arch = "x86_64", feature(stdarch_x86_lzcnt))]
+#![cfg_attr(target_arch = "x86_64", feature(stdarch_x86_popcnt))]
+#![cfg_attr(target_arch = "x86_64", feature(stdarch_x86_rdrand))]
+#![cfg_attr(target_arch = "x86_64", feature(stdarch_x86_rdseed))]
+#![cfg_attr(any(target_arch = "x86", target_arch = "x86_64"), feature(stdarch_x86_mmx))]
+#![feature(fmt_helpers_for_derive)]
+"#;
--- crates/intrinsic-test/src/x86/mod.rs
+++ crates/intrinsic-test/src/x86/mod.rs
@@ -5,9 +5,11 @@ mod xml_parser;
 
 use crate::common::SupportedArchitectureTest;
 use crate::common::cli::ProcessedCli;
+use crate::common::gen_rust::compile_rust_programs;
 use crate::common::intrinsic::{Intrinsic, IntrinsicDefinition};
 use crate::common::intrinsic_helpers::TypeKind;
-use crate::common::write_file::write_c_testfiles;
+use crate::common::write_file::{write_c_testfiles, write_rust_testfiles};
+use crate::x86::config::{F16_FORMATTING_DEF, X86_CONFIGURATIONS};
 use config::build_notices;
 use intrinsic::X86IntrinsicType;
 use xml_parser::get_xml_intrinsics;
@@ -67,7 +69,28 @@ impl SupportedArchitectureTest for X86ArchitectureTest {
     }
 
     fn build_rust_file(&self) -> bool {
-        todo!("build_rust_file in X86ArchitectureTest is not implemented")
+        // this is the module that handles the specific intrinsic
+        // within the core_arch crate in std::arch
+        let rust_target = if self.cli_options.target.contains("v7") {
+            "x86"
+        } else {
+            "x86_64"
+        };
+        let target = &self.cli_options.target;
+        let toolchain = self.cli_options.toolchain.as_deref();
+        let linker = self.cli_options.linker.as_deref();
+        let intrinsics_name_list = write_rust_testfiles(
+            self.intrinsics
+                .iter()
+                .map(|i| i as &dyn IntrinsicDefinition<_>)
+                .collect::<Vec<_>>(),
+            rust_target,
+            &build_notices("// "),
+            F16_FORMATTING_DEF,
+            X86_CONFIGURATIONS,
+        );
+
+        compile_rust_programs(intrinsics_name_list, toolchain, target, linker)
     }
 
     fn compare_outputs(&self) -> bool {
