6f9e90eecd0f83a9e5a209e0e138510a4eca61f6
Madhav Madhusoodanan <f20200049@pilani.bits-pilani.ac.in>
2025-09-16 16:13:31 +0530

    feat: updating intrinsic-run execution bash script

--- ci/run.sh
+++ ci/run.sh
@@ -187,6 +187,7 @@ case "${TARGET}" in
 
     x86_64-unknown-linux-gnu*)
         CPPFLAGS="${TEST_CPPFLAGS}" RUSTFLAGS="${HOST_RUSTFLAGS}" RUST_LOG=warn \
+            CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_RUNNER="true" \
             cargo run "${INTRINSIC_TEST}" "${PROFILE}"  \
             --bin intrinsic-test -- intrinsics_data/x86-intel.xml \
             --runner "${TEST_RUNNER}" \
