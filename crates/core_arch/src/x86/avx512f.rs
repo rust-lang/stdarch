eaad1297877a8670331ee296a813c605f6ffca1f
sayantn <sayantn05@gmail.com>
2025-10-02 00:42:57 +0530

    Add `_mm_sfence` to all non-temporal intrinsic tests

--- crates/core_arch/src/x86/avx512f.rs
+++ crates/core_arch/src/x86/avx512f.rs
@@ -56432,6 +56432,7 @@ mod tests {
         let mut mem = Memory { data: [-1.0; 16] };
 
         _mm512_stream_ps(&mut mem.data[0] as *mut f32, a);
+        _mm_sfence();
         for i in 0..16 {
             assert_eq!(mem.data[i], get_m512(a, i));
         }
@@ -56448,6 +56449,7 @@ mod tests {
         let mut mem = Memory { data: [-1.0; 8] };
 
         _mm512_stream_pd(&mut mem.data[0] as *mut f64, a);
+        _mm_sfence();
         for i in 0..8 {
             assert_eq!(mem.data[i], get_m512d(a, i));
         }
@@ -56464,6 +56466,7 @@ mod tests {
         let mut mem = Memory { data: [-1; 8] };
 
         _mm512_stream_si512(mem.data.as_mut_ptr().cast(), a);
+        _mm_sfence();
         for i in 0..8 {
             assert_eq!(mem.data[i], get_m512i(a, i));
         }
