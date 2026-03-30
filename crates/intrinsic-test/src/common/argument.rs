ffe700ceb86f86ac38c6ed0b820cf7f4eb3e30cb
Adam Gemmell <adam.gemmell@arm.com>
2026-03-30 12:16:49 +0100

    Fix duplicate const specifier

--- crates/intrinsic-test/src/common/argument.rs
+++ crates/intrinsic-test/src/common/argument.rs
@@ -30,8 +30,7 @@ where
     }
 
     pub fn to_c_type(&self) -> String {
-        let prefix = if self.ty.constant { "const " } else { "" };
-        format!("{prefix}{}", self.ty.c_type())
+        self.ty.c_type()
     }
 
     pub fn generate_name(&self) -> String {
